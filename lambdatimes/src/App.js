833c3c1d743a1b9ee73244833b7e595c71e90357
quansenB <inaki.breinbauer@gmail.com>
2019-06-07 11:05:37 +0200

    finish MVP functionaility

--- lambdatimes/src/App.js
+++ lambdatimes/src/App.js
@@ -1,8 +1,7 @@
-import React, { Component } from 'react';
+import React from 'react';
 import TopBar from './components/TopBar';
 import Header from './components/Header';
 import Content from './components/Content/Content';
-import CSS from "./CSS/index.css";
 
 const App = () => {
   return (
--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React from "react";
+import propTypes from "prop-types";
 
 const Tab = props => {
   /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
       if they match, the className should be: 'tab active-tab', 
       if it is not it should just be 'tab'*/
+  const getTabClass = selected => {
+    if (selected == props.tab) {
+      return "tab active-tab";
+    } else return "tab";
+  };
+
   return (
     <div
-      className={''}
+      className={getTabClass(props.selected)}
       onClick={() => {
         /* Replace this dummy click handler function with your selectTabHandler function from props 
          you'll need to pass the `tab` in as an argument to this handler. */
+        return props.selectTabHandler(props.tab);
       }}
     >
       {props.tab.toUpperCase()}
@@ -18,5 +26,9 @@ const Tab = props => {
 };
 
 // Make sure you include PropTypes on your props.
-
+Tab.propType = {
+  tab: propTypes.string.isRequired,
+  selectedTab: propTypes.string.isRequired,
+  selectTabHandler: propTypes.func.isRequired
+};
 export default Tab;
